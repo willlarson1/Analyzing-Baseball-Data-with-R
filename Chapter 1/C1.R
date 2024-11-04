44d68781d2235ea43bcb10e704e6a5bfbeccb2cb
Will Larson <willlarson@Wills-MacBook-Air.local>
2024-11-04 18:26:41 -0500

    Exercise 2 in C1

--- Chapter 1/C1.R
+++ Chapter 1/C1.R
@@ -8,3 +8,14 @@ crcblue <- "#2905a1"
 install.packages("Lahman")
 vignette("vignette-intro", package = "Lahman")
 
+# 1.10 Exercises
+
+# 1 - Which Datafile?
+# 1a - 
+
+# 2 - Lahman Pitching Data
+# 2a - Gibson completed 28/34 games that he started
+# 2b - Gibson had a 268:62 strikeout to walk ratio in 1968 (4.32:1)
+# 2c - Gibson pitched 304.2 innings in 1968
+# 2d - (198 hits + 62 walks)/304.6666... innings = 0.85 WHIP
+
