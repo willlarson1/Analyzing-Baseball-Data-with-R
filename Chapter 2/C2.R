a162bb01b21306197059ef2338f37b853e50a2ee
Will Larson <willlarson@Wills-MacBook-Air.local>
2024-11-11 13:58:51 -0500

    Chapter 2 through 2.9

--- Chapter 2/C2.R
+++ Chapter 2/C2.R
@@ -35,9 +35,9 @@ spahn <- spahn |>
 
 # arrange function sorts by column
 spahn |> 
-  arrange(FIP) |> 
-  select(Year, Age, W, L, ERA, FIP) |>
-  slice(1:6)
+  arrange(FIP) |> # sorts by FIP
+  select(Year, Age, W, L, ERA, FIP) |> # selects certain columns
+  slice(1:6) # takes top 6 rows
 
 # Good example of a pipeline
 # | - OR operator
@@ -52,4 +52,129 @@ spahn |>
     mean_FIP = mean(FIP)
   )
 
+# merging data frames - adding additional observations
+batting <- bind_rows(NLbatting, ALbatting)
+summary(batting)
+head(batting)
 
+# merging data frames - adding additional columns
+NL <- inner_join(NLbatting, NLpitching, by = "Tm")
+head(NL)
+
+#filtering data frame
+NL_150 <- NLbatting |> 
+  filter(HR > 150)
+
+head(NL_150)
+
+# 2.5 - Vectors
+# Spahn's wins and losses post-war
+W <- c(8, 21, 15, 21, 21, 22, 14)
+L <- c(5, 10, 12, 14, 17, 14, 19)
+win_pct <- 100 * W / (W + L)
+win_pct
+Year <- seq(from=1946, to=1952) # can also use colons: "Year <- 1946:1952
+
+Age <- Year - 1921
+
+# Plot function
+plot(Age, win_pct)
+
+# Various vector functions
+mean(win_pct)
+100 * sum(W) / (sum(W) + sum(L))
+sort(W)
+cumsum(W)
+summary(win_pct)
+
+# Extract values from vector
+W[c(1, 2, 5)]
+W[1 : 4]
+# Remove entries from vector
+W[-c(1, 6)]
+# Logical variables with a vector
+win_pct > 60
+(W > 20) & (win_pct > 60)
+
+win_pct == max(win_pct)
+Year[win_pct == max(win_pct)]
+Year[W + L > 30]
+
+# 2.6 - Objects and Containers in R
+Year <- 2008 : 2017
+NL <- c("PHI", "PHI", "SFN", "SLN", "SFN",
+        "SLN", "SFN", "NYN", "CHN", "LAN")
+AL <- c("TBA", "NYA", "TEX", "TEX", "DET",
+        "BOS", "KCA", "KCA", "CLE", "HOU")
+Winner <- c("NL", "AL", "NL", "NL", "NL",
+            "AL", "NL", "AL", "NL", "AL")
+N_Games <- c(5, 6, 5, 7, 4, 7, 7, 5, 7, 7)
+
+# Creating a data frame from multiple vectors
+WS_results <- tibble(
+  Year = Year, NL_Team = NL, AL_Team = AL,
+  N_Games = N_Games, Winner = Winner)
+WS_results
+
+grep("NY", c(AL, NL), value = TRUE)
+WS <- WS_results |> group_by(Winner) |> summarize(N = n())
+WS
+
+# Making a bar chart
+ggplot(WS, aes(x = Winner, y = N)) + geom_col()
+ggplot(WS_results, aes(x = Winner)) + geom_bar()
+
+WS_results |> 
+  group_by(NL_Team) |> 
+  summarize(N = n())
+
+WS_results <- WS_results |>
+  mutate(
+    NL_Team = factor(
+      NL_Team, 
+      levels = c("NYN", "PHI", "CHN", "SLN", "LAN", "SFN")
+    )
+  )
+
+str(WS_results$NL_Team)
+
+# Lists
+world_series <- list(
+  Winner = Winner, 
+  Number_Games = N_Games,
+  Seasons = "2008 to 2017"
+)
+world_series$Number_Games
+world_series[[2]]
+pluck(world_series, "Number_Games")
+world_series["Number_Games"]
+
+WS_results$NL_Team
+
+# 2.7
+library(Lahman)
+ws <- SeriesPost |>
+  filter(yearID >= 1903, round == "WS", wins + losses < 8)
+ggplot(ws, aes(x = wins + losses)) +
+  geom_bar(fill = crcblue) +
+  labs(x = "Number of games", y = "Frequency")
+
+hr_rates <- function(age, hr, ab) {
+  rates <- round(100 * hr / ab, 1)
+  list(x = age, y = rates)
+}
+
+HR <- c(13, 23, 21, 27, 37, 52, 34, 42, 31, 40, 54)
+AB <- c(341, 549, 461, 543, 517, 533, 474, 519, 541, 527, 514)
+Age <- 19 : 29
+hr_rates(Age, HR, AB)
+plot(hr_rates(Age, HR, AB))
+
+mantle_hr_rates <- hr_rates(Age, HR, AB)
+Mantle <- tibble(
+  Age, HR, AB, Rates = mantle_hr_rates$y
+)
+write_csv(Mantle, here::here("Chapter 2/mantle.csv"))
+list.files(here::here("Chapter 2"), pattern = "mantle")
+
+# 2.10 Splitting, Applying, and Combining Data
